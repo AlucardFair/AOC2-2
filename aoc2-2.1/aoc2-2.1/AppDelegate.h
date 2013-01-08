fa6831171a111ce7bfc72b8b8a192b43fb9dc3bd
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2013-01-07 19:32:22 -0600

    Initial Commit. Started a fresh build for AOC2 Project 1.

---
+++ aoc2-2.1/aoc2-2.1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  aoc2-2.1
+//
+//  Created by Zachery Hernandez on 1/7/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ aoc2-2.1/aoc2-2.1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aoc2-2.1
+//
+//  Created by Zachery Hernandez on 1/7/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
