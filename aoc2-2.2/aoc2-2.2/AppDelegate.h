0ad7fd83148de138a8a9e7dda3b8626ffb9e87b7
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2013-01-13 10:03:24 -0600

    Initial Commit for AOC2 Project 2. Started a fresh build.

---
+++ aoc2-2.2/aoc2-2.2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  aoc2-2.2
+//
+//  Created by Zachery Hernandez on 1/13/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ aoc2-2.2/aoc2-2.2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  aoc2-2.2
+//
+//  Created by Zachery Hernandez on 1/13/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
