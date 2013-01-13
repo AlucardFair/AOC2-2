e7df2ef20db3a063435ebf252ec93ab96ebdd859
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2013-01-13 12:54:06 -0600

    Added Second View with xib file named infoView.

---
+++ aoc2-2.2/aoc2-2.2/infoView.h
@@ -0,0 +1,13 @@
+//
+//  infoView.h
+//  aoc2-2.2
+//
+//  Created by Zachery Hernandez on 1/13/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface infoView : UIViewController
+
+@end
---
+++ aoc2-2.2/aoc2-2.2/infoView.m
@@ -0,0 +1,38 @@
+//
+//  infoView.m
+//  aoc2-2.2
+//
+//  Created by Zachery Hernandez on 1/13/13.
+//  Copyright (c) 2013 Zachery Hernandez. All rights reserved.
+//
+
+#import "infoView.h"
+
+@interface infoView ()
+
+@end
+
+@implementation infoView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
